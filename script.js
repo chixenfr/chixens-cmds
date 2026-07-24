00536081db9330b0790a59c864eed2163b72bd3a
chixen <chixenfr@gmail.com>
2026-07-24 16:48:50 -0700

    Add image slideshow and copy command functionality

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
+let images = [
 
+"images/iw8-1.png",
+
+"images/lumen-1.png",
+
+"images/iw8-2.png",
+
+"images/lumen-2.png"
+
+];
+
+
+let index = 0;
+
+
+setInterval(()=>{
+
+
+let slide=document.getElementById("slide");
+
+
+if(slide){
+
+index++;
+
+
+if(index >= images.length){
+
+index=0;
+
+}
+
+
+slide.src=images[index];
+
+
+}
+
+
+},4000);
+
+
+
+
+
+function copyCommand(id){
+
+
+let text=document.getElementById(id);
+
+
+text.select();
+
+
+navigator.clipboard.writeText(text.value);
+
+
+alert("Copied!");
+
+}
